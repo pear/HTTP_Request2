6177a1421d5f92a9d450d2800649633e63ef0770
Alexey Borzov <avb@php.net>
2009-08-14 15:40:22 +0000

    A few more comments
    git-svn-id: http://svn.php.net/repository/pear/packages/HTTP_Request2/trunk@287307 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/upload-rapidshare.php
+++ docs/examples/upload-rapidshare.php
@@ -2,6 +2,9 @@
 /**
  * Usage example for HTTP_Request2 package: uploading a file to rapidshare.com
  *
+ * Inspired by Perl usage example: http://images.rapidshare.com/software/rsapi.pl
+ * Rapidshare API description: http://rapidshare.com/dev.html
+ *
  * $Id$
  */
 
@@ -11,6 +14,7 @@ require_once 'HTTP/Request2.php';
 $filename = '/etc/passwd';
 
 try {
+    // First step: get an available upload server
     $request = new HTTP_Request2(
         'http://rapidshare.com/cgi-bin/rsapi.cgi?sub=nextuploadserver_v1'
     );
@@ -19,15 +23,19 @@ try {
         throw new Exception("Invalid upload server: {$server}");
     }
 
+    // Calculate file hash, we'll use it later to check upload
     if (false === ($hash = @md5_file($filename))) {
         throw new Exception("Cannot calculate MD5 hash of '{$filename}'");
     }
 
+    // Second step: upload a file to the available server
     $uploader = new HTTP_Request2(
         "http://rs{$server}l3.rapidshare.com/cgi-bin/upload.cgi",
         HTTP_Request2::METHOD_POST
     );
+    // Adding the file
     $uploader->addUpload('filecontent', $filename);
+    // This will tell server to return program-friendly output
     $uploader->addPostParameter('rsapi_v1', '1');
 
     $response = $uploader->send()->getBody();
@@ -38,6 +46,7 @@ try {
     foreach ($m as $item) {
         $rspAry[$item[1]] = $item[2];
     }
+    // Check that uploaded file has the same hash
     if (empty($rspAry['File1.4'])) {
         throw new Exception("MD5 hash data not found in response");
     } elseif ($hash != strtolower($rspAry['File1.4'])) {
